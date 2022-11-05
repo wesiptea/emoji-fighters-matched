2861ad2a78325035260a94db470f3f1f5e06c31f
Anne SK <92764640+wesiptea@users.noreply.github.com>
2022-11-05 12:04:16 -0700

    Update index.js
    Generating random integer and emoji added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let stageEl = document.getElementById("stage")
 let fightButton = document.getElementById("fightButton")
 
 fightButton.addEventListener("click", function() {
-    // Challenge:
-    // When the user clicks on the "Pick Fighters" button, pick two random 
-    // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
+   let randomIndexOne = Math.floor( Math.random() * fighters.length )
+   let randomIndexTwo = Math.floor( Math.random() * fighters.length )
+   stageEl.textContent = fighters[randomIndexOne] + " vs " + fighters[randomIndexTwo]
 })
